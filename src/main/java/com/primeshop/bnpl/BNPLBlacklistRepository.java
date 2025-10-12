9715f08e1ef70b716f4f85239ab75b5ced02ee5e
Trần Thành Hoài <tthoai654@gmail.com>
2025-10-12 14:10:07 +0700

    [add] bank and bnpl

---
+++ src/main/java/com/primeshop/bnpl/BNPLBlacklistRepository.java
@@ -0,0 +1,7 @@
+package com.primeshop.bnpl;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BNPLBlacklistRepository extends JpaRepository<BNPLBlacklist, Long> { }
+
+                                                                                                                        
